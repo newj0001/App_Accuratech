89c826b89de990314a1a770d2888115cb36aca28
Newjan Thevarajah <newj0001@student.eadania.dk>
2019-08-07 15:42:32 +0200

    first

--- CallWebApiConsole/Program.cs
+++ CallWebApiConsole/Program.cs
@@ -66,7 +66,7 @@ namespace CallWebApiConsole
             }
             else
             {
-                Console.WriteLine(result.StatusCode);//
+                Console.WriteLine(result.StatusCode);///
             }
         }
     }
