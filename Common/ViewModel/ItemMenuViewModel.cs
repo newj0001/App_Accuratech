f697f7e8b32ee79a3a0605fbf6bf04c54cfebaf5
Newjan Thevarajah <newj0001@student.eadania.dk>
2019-08-13 15:55:55 +0200

    Update

--- Common/ViewModel/ItemMenuViewModel.cs
+++ Common/ViewModel/ItemMenuViewModel.cs
@@ -1,18 +1,12 @@
-﻿using Common;
-using MaterialDesignThemes.Wpf;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Controls;
 
-namespace WPF.ViewModel
+namespace Common.ViewModel
 {
     public class ItemMenuViewModel : INotifyPropertyChanged
     {
@@ -26,10 +20,6 @@ namespace WPF.ViewModel
                 _menuItemsCollection = value;
                 NotifyPropertyChanged();
             }
-        }
-
-        public ItemMenuViewModel()
-        {
         }
 
         public async Task Reset()
