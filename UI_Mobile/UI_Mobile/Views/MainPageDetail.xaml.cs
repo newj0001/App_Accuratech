71cde22d6157c170ba3be17cefaee982d8be7391
Newjan Thevarajah <newj0001@student.eadania.dk>
2019-09-30 15:03:15 +0200

    barcode update

--- UI_Mobile/UI_Mobile/Views/MainPageDetail.xaml.cs
+++ UI_Mobile/UI_Mobile/Views/MainPageDetail.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using Common;
+using Common.Services;
 using Honeywell.AIDC.CrossPlatform;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using UI_Mobile.ViewModels;
 using Xamarin.Forms;
@@ -14,22 +16,44 @@ namespace UI_Mobile.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPageDetail : ContentPage
     {
-        private string _scannerName;
-        private BarcodeReader _barcodeReader = new BarcodeReader(null);
+        private readonly RegistrationValueDataStore _registrationValueDataStore;
+        private readonly SubItemEntityModel _parentSubItem;
 
-        protected override void OnAppearing()
+        private const string DEFAULT_READER_KEY = "default";
+        private Dictionary<string, BarcodeReader> mBarcodeReaders;
+        private bool mContinuousScan = false, mOpenReader = false;
+        private BarcodeReader mSelectedReader = null;
+        private SynchronizationContext mUIContext = SynchronizationContext.Current;
+        private int mTotalContinuousScanCount = 0;
+        private bool mSoftContinuousScanStarted = false;
+        private bool mSoftOneShotScanStarted = false;
+        private string deviceModel = null;
+        private BarcodeReaderInfo reader;
+        private string _scannerName;
+
+
+        public MainPageDetail(MenuItemEntityModel menuItemEntityModel)
         {
-            //_barcodeReader.BarcodeDataReady += MBarcodeReader_BarcodeDataReady;
+            InitializeComponent();
+            ClearText(menuItemEntityModel);
+            var fieldItemsViewModel = new FieldItemsViewModel();
+            fieldItemsViewModel.Reset(menuItemEntityModel);
+            BindingContext = fieldItemsViewModel;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
         }
 
         protected override void OnDisappearing()
         {
-            _barcodeReader.BarcodeDataReady -= MBarcodeReader_BarcodeDataReady;
+            //_barcodeReader.BarcodeDataReady -= MBarcodeReader_BarcodeDataReady;
         }
 
-        private async void PopulateReaderPicker()
+        private async void PopulateReader()
         {
-            _barcodeReader = null;
             // Queries the list of readers that are connected to the mobile computer.
             IList<BarcodeReaderInfo> readerList = await BarcodeReader.GetConnectedBarcodeReaders();
             if (readerList.Count > 0)
@@ -44,24 +68,8 @@ namespace UI_Mobile.Views
                 _scannerName = "";
             }
         }
+ 
 
-        public MainPageDetail(MenuItemEntityModel menuItemEntityModel)
-        {
-            InitializeComponent();
-            ClearText(menuItemEntityModel);
-            var fieldItemsViewModel = new FieldItemsViewModel();
-
-            
-            fieldItemsViewModel.Reset(menuItemEntityModel);
-            BindingContext = fieldItemsViewModel;
-
-            PopulateReaderPicker();
-        }
-        private void MBarcodeReader_BarcodeDataReady(object sender, BarcodeDataArgs e)
-        {
-
-        }
-        
         private async void OnItemSelected(object sender, ItemTappedEventArgs e)
         {
             var selectedItem = e.Item as SubItemEntityModel;
@@ -71,6 +79,7 @@ namespace UI_Mobile.Views
                 return;
             }
         }
+
         private async void SaveClicked(object sender, EventArgs e)
         {
             var subItems = ((ListView)SubItemsListView).ItemsSource;
@@ -101,5 +110,7 @@ namespace UI_Mobile.Views
                 item.FieldValue = "";
             }
         }
+
+        public event EventHandler NewRegistrationValueCreated;
     }
 }
